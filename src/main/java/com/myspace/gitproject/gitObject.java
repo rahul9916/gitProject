c573cdc52bb7fc4f11c04c76bb419a897c998cb0
krisv <>
2020-10-01 13:13:34 +0530

    {/src/main/java/com/myspace/gitproject/gitObject.java}

--- src/main/java/com/myspace/gitproject/gitObject.java
+++ src/main/java/com/myspace/gitproject/gitObject.java
@@ -6,12 +6,24 @@ package com.myspace.gitproject;
 
 public class gitObject implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public gitObject() {
-    }
+	@org.kie.api.definition.type.Label(value = "name")
+	private java.lang.String name;
 
+	public gitObject() {
+	}
 
+	public java.lang.String getName() {
+		return this.name;
+	}
 
+	public void setName(java.lang.String name) {
+		this.name = name;
+	}
+
+	public gitObject(java.lang.String name) {
+		this.name = name;
+	}
 
 }
